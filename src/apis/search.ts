1131216ca97fb01dd85042b24de96bc6a7d1514a
Taekjin Jang <xorwls1500@naver.com>
2023-09-06 11:25:56 +0900

    feat : api호출에 캐시 적용

--- src/apis/search.ts
+++ src/apis/search.ts
@@ -1,6 +1,10 @@
-import {instance} from './instasnce';
+import {instance} from 'apis/instasnce';
+import * as cacheStorage from 'utils/cacheStorage';
 
 export const getRecommendSearch = async (search: string) => {
+    const cachedResponse = await cacheStorage.getCache(search);
+    if (cachedResponse) return cachedResponse.json();
     const response = await instance.get(`/sick?q=${search}`);
+    if (response.data.length > 0) await cacheStorage.setCache(search, response.data);
     return response.data;
 };
