9099ae1d306c27c4d5ae37c27cce39be6fb38286
Taekjin Jang <xorwls1500@naver.com>
2023-09-08 11:19:49 +0900

    refactor: 자음,모음 제거 함수 수정 및 폴더 변경

--- src/hooks/useRecsSearch.ts
+++ src/hooks/useRecsSearch.ts
@@ -3,6 +3,7 @@ import {MAX_RECOMMEND_NUM} from 'constants/constants';
 import {useCallback, useState} from 'react';
 import {searchItemType} from 'types/search';
 import * as cacheStorage from 'utils/cacheStorage';
+import {replaceValidKeyword} from 'utils/regex';
 
 const useRecsSearch = () => {
     const [recsSearchList, setRecsSearchList] = useState<searchItemType[]>([]);
@@ -11,15 +12,15 @@ const useRecsSearch = () => {
     const getRecsSearch = useCallback(async (search: string) => {
         try {
             setIsLoading(true);
-            const isKoreanJamo = (str: string) => /[ㄱ-ㅎㅏ-ㅣ]/.test(str);
-            if (!isKoreanJamo(search) && search.length > 0) {
-                const cachedResponse = await cacheStorage.getCache(search);
+            const regexSearch = replaceValidKeyword(search);
+            if (regexSearch.length > 0) {
+                const cachedResponse = await cacheStorage.getCache(regexSearch);
                 const res = cachedResponse
                     ? await cachedResponse.json()
-                    : await getRecommendSearch(search);
+                    : await getRecommendSearch(regexSearch);
                 const sliceRes =
                     res.length > MAX_RECOMMEND_NUM ? res.slice(0, MAX_RECOMMEND_NUM) : res;
-                if (!cachedResponse) await cacheStorage.setCache(search, sliceRes);
+                if (!cachedResponse) await cacheStorage.setCache(regexSearch, sliceRes);
                 setRecsSearchList(sliceRes);
             } else setRecsSearchList([]);
         } catch (error) {
